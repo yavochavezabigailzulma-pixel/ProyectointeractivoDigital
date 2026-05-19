9efe8c17d3c09b0365aab3659b06e66a31a21995
el-Marce <gallardocoriamarcelodaniel@gmail.com>
2026-05-19 10:36:36 -0400

    Navegabilidad panel estrellas

--- Assets/Scripts/CuerposCelestes/NavegadorCuerpos.cs
+++ Assets/Scripts/CuerposCelestes/NavegadorCuerpos.cs
@@ -1,7 +1,9 @@
+using TMPro;
 using UnityEngine;
 
 public class NavegadorCuerpos : MonoBehaviour
 {
+    public GameObject canvas;
     //public static NavegadorCuerpos Instance;
 
     public GameObject pantallaMenu;
@@ -15,6 +17,15 @@ public class NavegadorCuerpos : MonoBehaviour
     public GameObject bienvenidaEstrellas;
     public GameObject bienvenidaGalaxias;
     public GameObject bienvenidaPlanetas;
+
+    public GameObject nebulosa;
+    //public GameObject nebulosaContenido;
+    public GameObject secuenciaPrincipal;
+    //public GameObject secuenciaContenido;
+    public GameObject muerteEstrella;
+    //public GameObject muerteEstrContenido;
+    public GameObject estrellaInfo;
+    public TextMeshProUGUI textoInfo;
 
     public void AbrirSeccion(int index)
     {
@@ -55,5 +66,55 @@ public class NavegadorCuerpos : MonoBehaviour
             bienvenidaGalaxias.SetActive(false);
         if (bienvenidaPlanetas)
             bienvenidaPlanetas.SetActive(false);
+    }
+
+    public void Ir3D(int tipo)
+    {
+        canvas.SetActive(false);
+        
+        switch (tipo){
+            case 1: nebulosa.SetActive(true); break;
+            case 2: secuenciaPrincipal.SetActive(true); break;
+            case 3: muerteEstrella.SetActive(true); break;
+        }
+    }
+
+    public void Volver3D(int tipo)
+    {
+        canvas.SetActive(true);
+
+        switch (tipo)
+        {
+            case 1: nebulosa.SetActive(false); break;
+            case 2: secuenciaPrincipal.SetActive(false); break;
+            case 3: muerteEstrella.SetActive(false); break;
+        }
+    }
+
+    public void mostrarInfo(int tipo)
+    {
+        estrellaInfo.SetActive(true);
+        switch (tipo)
+        {
+            case 1: 
+                textoInfo.text =
+                    "<b> 1.\tNebulosa. </b>\n" +
+                    "\n" +
+                    "Las estrellas nacen en enormes nubes de gas y polvo llamadas nebulosas. Con el tiempo, la gravedad junta estos materiales hasta formar una nueva estrella."; break;
+            case 2:
+                textoInfo.text =
+                    "<b> 2.\tSecuencia Principal. </b>\n" +
+                    "\n" +
+                    "Es la etapa más larga de la vida de una estrella. Durante este tiempo, produce luz y calor, como nuestro Sol."; break;
+            case 3:
+                textoInfo.text =
+                    "<b> 3.\tMuerte. </b>\n" +
+                    "\n" +
+                    "Cuando una estrella agota su energía, cambia según su tamaño. Algunas se convierten en enanas blancas y otras explotan formando supernovas o agujeros negros."; break;
+        }
+    }
+    public void ocultarInfo()
+    {
+        estrellaInfo.SetActive(false);
     }
 }
